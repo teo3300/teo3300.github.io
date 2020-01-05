6cd960e5d0c449de59c51568946ae296ab107b6c
teo3300 <matteo.rogora@live.it>
2020-01-05 21:17:09 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -44,19 +44,17 @@ function formatContent() {
   while(!text[srt]) srt++;
   document.getElementById("page_title").innerHTML = createUrl(text[srt]);
   for(var i = srt+1; i < text.length; i++){
-    if(text[i]){
-      if(text[i].includes("](")){
-        text[i] = createUrl(text[i]);
-      }
+    if(text[i].includes("](")){
+      text[i] = createUrl(text[i]);
+    }
     /*if(text[i].includes("**")){
       text[i] = toBold(text[i]);
     }*/
-      if(text[i].substr(0,3) == "## "){
-        text[i] = "<h2>" + text[i].substr(3) + "</h2>\n";
-      }else{
-        text[i] = "<p>" + text[i] + "</p>\n";
-      }
-      document.getElementById("sheet").innerHTML += text[i];
+    if(text[i].substr(0,3) == "## "){
+      text[i] = "<h2>" + text[i].substr(3) + "</h2>\n";
+    }else{
+      text[i] = "<p>" + text[i] + "</p>\n";
     }
+    document.getElementById("sheet").innerHTML += text[i];
   }
 }
