b383a7c105d29e08b905d3cb5f1c8ddc381ab268
teo3300 <matteo.rogora@live.it>
2022-04-05 11:28:25 +0200

    moving from substr (deprecated) to substring

--- logic.js
+++ logic.js
@@ -1,30 +1,30 @@
 function createUrl(line) {
-    var i1, i2, i3;
+    var openAltPos, closeAltPos, closeLinkPos;
     var open = false;
     var cnt = 0;
     var qnt = 0;
     for (var a = 0; a < line.length; a++) {
         if (line[a] == "[") {
-            i1 = a;
+            openAltPos = a;
             open = true;
             for (var b = a; b < line.length && open; b++) {
                 if (line[b] == "]" && line[b + 1] == "(") {
-                    i2 = b;
+                    closeAltPos = b;
                     if (qnt == 1) {
                         for (var c = b; c < line.length && open; c++) {
                             if (line[c] == ")") {
-                                i3 = c;
+                                closeLinkPos = c;
                                 if (cnt == 1) {
-                                    if (line[i1 - 1] == "!" || line[i1 - 1] == "?") {
+                                    if (line[openAltPos - 1] == "!" || line[openAltPos - 1] == "?") {
                                         var klass;
-                                        if (line[i1 - 1] == "?")
+                                        if (line[openAltPos - 1] == "?")
                                             klass = "inLineImg";
                                         else {
                                             klass = "innerImg";
                                         }
-                                        line = line.substr(0, i1 - 1) + "<img class=\"" + klass + "\" src=\"" + line.substr(i2 + 2, i3 - i2 - 2) + "\" alt=\"" + line.substr(i1 + 1, i2 - i1 - 1) + "\">" + line.substr(i3 + 1);
+                                        line = line.substring(0, openAltPos - 1) + "<img class=\"" + klass + "\" src=\"" + line.substring(closeAltPos + 2, closeLinkPos) + "\" alt=\"" + line.substring(openAltPos + 1, closeAltPos) + "\">" + line.substring(closeLinkPos + 1);
                                     } else {
-                                        line = line.substr(0, i1) + "<a href=\"" + line.substr(i2 + 2, i3 - i2 - 2) + "\">" + line.substr(i1 + 1, i2 - i1 - 1) + "</a>" + line.substr(i3 + 1);
+                                        line = line.substring(0, openAltPos) + "<a href=\"" + line.substring(closeAltPos + 2, closeLinkPos) + "\">" + line.substring(openAltPos + 1, closeAltPos) + "</a>" + line.substring(closeLinkPos + 1);
                                     }
                                 }
                                 cnt--;
@@ -67,37 +67,37 @@ function formatContent() {
         /*if(text[i].includes("**")){
           text[i] = toBold(text[i]);
         }else */
-        if (text[i].substr(0, TAG_TITLE.length) == TAG_TITLE) {
-            var sub_title = text[i].substr(TAG_TITLE.length);
+        if (text[i].substring(0, TAG_TITLE.length) == TAG_TITLE) {
+            var sub_title = text[i].substring(TAG_TITLE.length);
             text[i] = "<h2 id=\"" + sub_title.replace(" ", "-") + "\">" + sub_title + "</h2>\n";
 
-        } else if (text[i].substr(0, TAG_LIST.length) == TAG_LIST) {
-            text[i] = "<li>" + text[i].substr(TAG_LIST.length) + "</li>\n";
+        } else if (text[i].substring(0, TAG_LIST.length) == TAG_LIST) {
+            text[i] = "<li>" + text[i].substring(TAG_LIST.length) + "</li>\n";
             /*************************************************/
-        } else if (text[i].substr(0, TAG_BLOCK_START.length) == TAG_BLOCK_START) {
-            if (text[i].substr(text[i].length - TAG_BLOCK_END.length, TAG_BLOCK_END.length) == TAG_BLOCK_END) {
-                text[i] = "<div class=\"in-block\">\n" + text[i].substr(TAG_BLOCK_END.length, text[i].length - (TAG_BLOCK_START.length + TAG_BLOCK_END.length)) + "</div>";
+        } else if (text[i].substring(0, TAG_BLOCK_START.length) == TAG_BLOCK_START) {
+            if (text[i].substring(text[i].length - TAG_BLOCK_END.length) == TAG_BLOCK_END) {
+                text[i] = "<div class=\"in-block\">\n" + text[i].substring(TAG_BLOCK_END.length, text[i].length - TAG_BLOCK_END.length) + "</div>";
             } else {
                 text[i] = ""; // Boh, robe
-                for (; i < text.length && text[i].substr(text[i].length - TAG_BLOCK_END.length, TAG_BLOCK_END.length) != TAG_BLOCK_END; ++i) {
+                for (; i < text.length && text[i].substring(text[i].length - TAG_BLOCK_END.length) != TAG_BLOCK_END; ++i) {
                     if (text[i].includes("](")) {
                         block += createUrl(text[i]);
                     } else {
                         block += (text[i] + "\n");
                     }
                 }
-                if (text[i].substr(text[i].length - TAG_BLOCK_END.length, TAG_BLOCK_END.length) == TAG_BLOCK_END) {
+                if (text[i].substring(text[i].length - TAG_BLOCK_END.length) == TAG_BLOCK_END) {
                     block += "</div>"
                 }
                 text[i] = block;
             }
-        } else if (text[i].substr(0, TAG_GALLERY.length) == TAG_GALLERY) {
-            var res = text[i].split(TAG_GALLERY_SEP)[0].substr(TAG_GALLERY.length);
+        } else if (text[i].substring(0, TAG_GALLERY.length) == TAG_GALLERY) {
+            var res = text[i].split(TAG_GALLERY_SEP)[0].substring(TAG_GALLERY.length);
             var legend = text[i].split(TAG_GALLERY_SEP)[1];
             if (legend == undefined) legend = "";
             text[i] = block + "<a href=\"" + res + "\"><img class=\"innerImg\" src=\"" + res + "\" alt=\"" + res + "\"> </a><br><p>" + legend + "</p></div>";
         } /*************************************************/
-        else if (text[i].substr(0, 1) != "<") {
+        else if (text[i].substring(0, 1) != "<") {
             text[i] = "<p>" + text[i] + "</p>\n";
         }
         document.getElementById("sheet").innerHTML += text[i];
